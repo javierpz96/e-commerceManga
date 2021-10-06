cc06a024906f1563fb5efb27ef7297fe5fa82ada
Javier <75548156+javierpz96@users.noreply.github.com>
2021-10-06 14:19:11 -0300

    Agregando imagenes del mapeo

--- src/components/itemList/ItemList.js
+++ src/components/itemList/ItemList.js
@@ -2,7 +2,7 @@ import Item from './item/Item'
 import ItemCount from '../button/ItemCount'
 import '../Imagenes/i1.jpg'
 import React,{useState,useEffect} from 'react'
-
+import './item/ItemList.css'
 
 const data = [
     {   id:1,
@@ -10,16 +10,16 @@ const data = [
         descripcion:'La historia de dos adolescentes que tienen sexo sin amor para llegar a sentir algo, se mezcla con el desinterés de los adultos, la muerte de un miembro de la familia y unos pasajes con la belleza y el vuelo poético al que nos tiene acostumbrados este aclamado autor.',
         precio: 990,
         idioma: 'Castellano',
-        imagen: 'i1'
+        imagen: 'https://d3ugyf2ht6aenh.cloudfront.net/stores/399/159/products/lachicaalaorilladelmar-cf770bf22551ccd45316220794913375-480-0.jpg'
         
     },
     { 
         id:2,
         nombre:'El Fin Del Mundo Y Antes Del Amanecer',
-        descripcion: 'MAGNÍFICO RECOPILATORIO DE HISTORIAS CORTAS DEL GENIAL INIO ASANO Una estudiante de instituto que quiere dejar su pequeña ciudad, un oficinista de mediana edad que se queda en paro y se busca a sí mismo, una chica que se replantea su vida varios años tras acabar su adolescencia, un mangaka que vuelve a su pueblo natal... Relatos cortos llenos de la magia y la sensibilidad de Inio Asano. ',
+        descripcion: 'MAGNÍFICO RECOPILATORIO DE HISTORIAS CORTAS DEL GENIAL INIO ASANO Una estudiante de instituto que quiere dejar su pequeña ciudad, un oficinista de mediana edad que se queda en paro y se busca a sí mismo, una chica que se replantea su vida varios años tras acabar su adolescencia, un mangaka que vuelve a su pueblo natal...',
         precio: 1500,
         idioma: 'Castellano',
-        imagen: '/src/components/Imagenes/53fb7b1cb4bb089c31e01843bc05972c--lain-serial-experiments-lookbook.jpg'
+        imagen: 'https://www.normaeditorial.com/upload/media/albumes/0001/06/thumb_5041_albumes_big.jpeg'
     },
     {
         id:3,
@@ -27,7 +27,7 @@ const data = [
         descripcion : 'Los vecinos del Barrio de la luz pueden parecer personas cualquiera, como tú y como yo. Y en cierto modo lo son. Sin embargo, si miras a través de los ojos de Inio Asano, sus historias resultarán ser más conmovedoras y únicas de lo que a priori puede parecer. Otro genial recopilatorio de historias cortas del autor que mejor capta las inquietudes de las nuevas generaciones. ',
         precio: '1360',
         idioma: 'Castellano',
-        imagen: ''
+        imagen: 'https://www.normaeditorial.com/upload/media/albumes/0001/06/f9e0d6b000c4ad5bd93bb485c05ce4d32aa8c7d3.jpeg'
     }
 ]
 
@@ -64,7 +64,7 @@ const ItemList = () => {
             
             productos.map((items)=>
             
-            <div className='border border-secondary card m-3'>
+            <div className='border border-secondary card m-4'>
                 <div className="card-body">
                     <Item  key={items.key} nombre={items.nombre} descripcion ={items.descripcion} precio={items.precio} imagen={items.imagen} />
                 </div>
--- src/components/itemList/item/Item.js
+++ src/components/itemList/item/Item.js
@@ -1,19 +1,25 @@
-import React from 'react'
-import ItemCount from '../../button/ItemCount'
+import React from "react";
+import ItemCount from "../../button/ItemCount";
+import "./ItemList.css";
 
-const Item = ({nombre,descripcion,precio,idioma,id,imagen}) => {
-    return (
-        <div className='item'>   
-            <h2>{nombre}</h2>
-            <img src={imagen} alt="imagen" />  
-            <h4>{descripcion}</h4>
-            <h2>${precio}</h2>
-            <h4>{idioma}</h4>
-            <h2>{id}</h2>
-            <ItemCount stock ='30' initial='0'></ItemCount>
-            
-        </div>
-    )
-}
+const Item = ({ nombre, descripcion, precio, idioma, id, imagen }) => {
+  return (
+    <div className="item">
+      <h2>{nombre}</h2>
 
-export default Item
+      <img src={imagen} alt="imagen" />
+
+      <p>{descripcion}</p>
+      <div className="precio">
+        <h2>${precio}</h2>
+      </div>
+      <h4>{idioma}</h4>
+      <h2>{id}</h2>
+      <div className="contador">
+        <ItemCount stock="30" initial="0"></ItemCount>
+      </div>
+    </div>
+  );
+};
+
+export default Item;
